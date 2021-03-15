82d1afc16b31b817ded02ba2aab05da8ae291edb
Eduardo Alfaro <eduardo@Eduardos-MacBook-Pro.local>
2021-03-14 20:18:54 -0600

    admin bro custom configuration

--- src/router/AdminBroRouter.js
+++ src/router/AdminBroRouter.js
@@ -5,9 +5,27 @@ import db from '../models'
 
 AdminBro.registerAdapter(AdminBroSequelize)
 
+const userProperties = ['id', 'firstName', 'lastName', 'email']
+const commonProperties = ['createdAt', 'updatedAt']
+
+const accountNavigation = {
+  name: 'General',
+  icon: 'Account',
+}
+
 export const adminBro = new AdminBro({
   // databases: [db],
-  resources: [{ resource: db.User }],
+  resources: [
+    {
+      resource: db.User,
+      options: {
+        listProperties: [...userProperties, ...commonProperties],
+        editProperties: userProperties.filter((property) => property !== 'id'),
+        filterProperties: [...userProperties, ...commonProperties],
+        navigation: accountNavigation,
+      },
+    },
+  ],
   rootPath: '/admin',
   branding: {
     companyName: process.env.API_NAME,

62d3a986f4b0a4d7f2b5e4774abdd0c996f4d977
Eduardo Alfaro <eduardo@Eduardos-MacBook-Pro.local>
2021-03-14 19:43:10 -0600

    remove UserRouter

--- src/api/v1/utils/generateRouter.js
+++ src/api/v1/utils/generateRouter.js
@@ -6,7 +6,7 @@ const generateRouter = (controller) => {
   if (controller.getAll) router.get('/', controller.getAll)
   if (controller.create) router.post('/', controller.create)
   if (controller.getById) router.get('/:id', controller.getById)
-  if (controller.update) router.patch('/:id', controller.update)
+  if (controller.update) router.put('/:id', controller.update)
   if (controller.delete) router.delete('/:id', controller.delete)
 
   return router
